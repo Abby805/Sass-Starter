b43542e4621784b9523ed139b74b328cf14e3ad4
Abby Milberg <abby805@gmail.com>
2015-12-29 14:43:53 -0500

    Adding cssnano to minify.

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,8 @@ module.exports = function (grunt) {
       options: {
         map: true,
         processors: [
-          require('autoprefixer')({browsers: ['last 2 versions']})
+          require('autoprefixer')({browsers: ['last 2 versions']}),
+          require('cssnano')()
         ]
       },
       dev: {
